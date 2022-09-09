54398d6cc339599e56c70696a07393648cc17c41
Sarah-F <60114072+Sarah-F@users.noreply.github.com>
2022-09-09 14:51:16 -0400

    Update portscanner.sh

--- Week 2/Lab 2.1 Port Scanning 1/portscanner.sh
+++ Week 2/Lab 2.1 Port Scanning 1/portscanner.sh
@@ -7,7 +7,7 @@ echo "host,port" > results.txt
 for host in $(cat $hostfile); do
   for port in $(cat $portfile); do
     timeout .1 bash -c "echo >/dev/tcp/$host/$port" 2>/dev/null &&
-      echo "$host,$port" >> results.txt
+      echo "$host,$port" > results.txt
   done
   echo #creates space between each IP address
 done
