5e85792950767858620571f1f4143d89fd41dc54
Sarah-F <60114072+Sarah-F@users.noreply.github.com>
2022-09-13 11:06:59 -0400

    Update portscanner2.sh

--- Week 3/portscanner2.sh
+++ Week 3/portscanner2.sh
@@ -1,20 +1,11 @@
 #!/bin/bash
 
-network_prefix=$1
+ip=$1
 port=$2
-start=1
-end=254
+
 echo "ip,port"
 
-for ip in $(10.0.5/24); do
-  for port in $(53); do
+for ip in $(seq 0 254); do 
     timeout .1 bash -c "echo >/dev/tcp/$ip/$port" 2>/dev/null &&
       echo "$ip,$port"
-  done
-  echo #creates space between each IP address
 done
-
-
-sudo ping -c 1 -g 10.0.5.0/24 -p 53
-
-sudo nmap 10.0.5/24 -p 53 | cut -d " " -f 5 >> portScan2.txt
