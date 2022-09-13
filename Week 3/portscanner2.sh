151fe17a902a557a24c8c4267d057a298520bd63
Sarah-F <60114072+Sarah-F@users.noreply.github.com>
2022-09-13 11:15:06 -0400

    Update portscanner2.sh

--- Week 3/portscanner2.sh
+++ Week 3/portscanner2.sh
@@ -5,7 +5,7 @@ port=$2
 
 echo "ip,port"
 
-for ip in $(seq 0 254); do 
-    timeout .1 bash -c "echo >/dev/tcp/$ip/$port" 2>/dev/null &&
-      echo "$ip,$port"
+for i in $(seq 0 254); do 
+    timeout .1 bash -c "echo >/dev/tcp/$ip.$i/$port" 2>/dev/null &&
+      echo "$ip.$i,$port"
 done
