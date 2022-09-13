3e5e2bc04941032b172db167a9c704ab08336f85
Sarah-F <60114072+Sarah-F@users.noreply.github.com>
2022-09-13 10:41:33 -0400

    Update portscanner2.sh

--- Week 3/portscanner2.sh
+++ Week 3/portscanner2.sh
@@ -1,13 +1,25 @@
 #!/bin/bash
-# This version of code is made by Sarah F. and John T.
-It puts the full output into a text file, and creates spaces between the different IP addresses.
+
+network_prefix=$1
+port=$2
+start=1
+end=254
+echo "ip,port"
+
+
+
 hostfile=$1
 portfile=$2
-echo "host,port" > results.txt
-for host in $(cat $hostfile); do
-  for port in $(cat $portfile); do
+echo "host,port"
+for host in $(cat $network_prefix); do
+  for port in $(cat $port); do
     timeout .1 bash -c "echo >/dev/tcp/$host/$port" 2>/dev/null &&
-      echo "$host,$port" > results.txt
+      echo "$host,$port"
   done
   echo #creates space between each IP address
 done
+
+
+sudo ping -c 1 -g 10.0.5.0/24 -p 53
+
+sudo nmap 10.0.5/24 -p 53 | cut -d " " -f 5 >> portScan2.txt
