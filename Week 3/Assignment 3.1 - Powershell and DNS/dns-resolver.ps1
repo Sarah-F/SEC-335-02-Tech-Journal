20718b83e5368199eca6d4fb3485009df7b27145
Sarah-F <60114072+Sarah-F@users.noreply.github.com>
2022-09-14 17:11:03 -0400

    Create dns-resolver.ps1

---
+++ Week 3/Assignment 3.1 - Powershell and DNS/dns-resolver.ps1
@@ -0,0 +1,12 @@
+#!/bin/bash
+param($network, $server)
+  for ($i = 0; $i -lt 255; $i++)
+  {
+  $ip = "$network" + "." +  "$i"
+  $result = Resolve-DnsName -DnsOnly $ip -Server $server -ErrorAction Ignore | Select-Object -Property NameHost
+  if ($result  -like "*")
+    { 
+        echo "IP: " $ip "namehost:" $result
+    }
+  }
+done
