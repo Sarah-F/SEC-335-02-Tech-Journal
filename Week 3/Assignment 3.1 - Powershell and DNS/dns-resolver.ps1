444619c4139ad45486763cb759a100ad6e23a396
Sarah-F <60114072+Sarah-F@users.noreply.github.com>
2022-09-15 22:09:28 -0400

    Update dns-resolver.ps1

--- Week 3/Assignment 3.1 - Powershell and DNS/dns-resolver.ps1
+++ Week 3/Assignment 3.1 - Powershell and DNS/dns-resolver.ps1
@@ -1,11 +1,7 @@
-param($network, $server)
-  for ($i = 0; $i -lt 255; $i++)
+param ($network, $server)
+$range = 1..254
+  for each($r in $range)
   {
-  $ip = "$network" + "." +  "$i"
-  $result = Resolve-DnsName -DnsOnly $ip -Server $server -ErrorAction Ignore | Select-Object -Property NameHost
-  if ($result  -like "*")
-    { 
-        echo "IP: " $ip "namehost:" $result
-    }
+    Resolve-DnsName -DnsOnly "$network.$r" -Server "$server" -ErrorAction Ignore
   }
 done
