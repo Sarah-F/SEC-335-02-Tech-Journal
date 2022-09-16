51daa79e48a98940f91487fc13b4893b1e0beae5
Sarah-F <60114072+Sarah-F@users.noreply.github.com>
2022-09-15 22:26:48 -0400

    Update dns-resolver.ps1

--- Week 3/Assignment 3.1 - Powershell and DNS/dns-resolver.ps1
+++ Week 3/Assignment 3.1 - Powershell and DNS/dns-resolver.ps1
@@ -4,3 +4,12 @@ $range = 1..254
   {
     Resolve-DnsName -DnsOnly "$network.$r" -Server "$server" -ErrorAction Ignore
   }
+
+param ($net, $dnsserver)
+$range = 1..254
+foreach($r in $range)
+{
+
+Resolve-DnsName -DnsOnly "$net.$r" -Server "$dnsserver" -ErrorAction Ignore
+
+}
