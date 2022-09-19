d985e7a51923e9faad0446880ac3c9b1049f0bcd
Sarah-F <60114072+Sarah-F@users.noreply.github.com>
2022-09-18 21:09:58 -0400

    Update dns-resolver.ps1

--- Week 3/Assignment 3.1 - Powershell and DNS/dns-resolver.ps1
+++ Week 3/Assignment 3.1 - Powershell and DNS/dns-resolver.ps1
@@ -4,3 +4,14 @@ foreach($r in $range)
 {
   Resolve-DnsName -DnsOnly "$network.$r" -Server "$server" -ErrorAction Ignore | Select-Object -Property Name , NameHost
 }
+
+param ($network, $server)
+for ($i = 0; $i -le 256; $i++) 
+{
+    $ip = ($network + "." + $i)
+    $hostname = (Resolve-DnsName -DnsOnly $ip -Server $server -ErrorAction Ignore)
+    if ($hostname -ne $null) 
+    {
+        Write-Host $ip $hostname.NameHost
+    }
+}
